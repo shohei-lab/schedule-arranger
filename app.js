223b9c7756890b1b220d292762b3703c19a1e2ac
Shohei Miyahara <m.shohei1216@gmail.com>
2021-02-19 09:26:07 +0900

    Herokuの環境変数を利用できるように修正

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ User.sync().then(() => {
 });
 
 var GitHubStrategy = require('passport-github2').Strategy;
-var GITHUB_CLIENT_ID = '1332897224cc4e502014';
-var GITHUB_CLIENT_SECRET = 'be36966477fbe34236f516a64172b18b0e9f2252';
+var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '1332897224cc4e502014';
+var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || 'be36966477fbe34236f516a64172b18b0e9f2252';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
@@ -41,7 +41,7 @@ passport.deserializeUser(function (obj, done) {
 passport.use(new GitHubStrategy({
   clientID: GITHUB_CLIENT_ID,
   clientSecret: GITHUB_CLIENT_SECRET,
-  callbackURL: 'http://localhost:8000/auth/github/callback'
+  callbackURL: process.env.HEROKU_URL ? process.env.HEROKU_URL + 'auth/github/callback' :  'http://localhost:8000/auth/github/callback'
 },
 function (accessToken, refreshToken, profile, done) {
   process.nextTick(function () {
